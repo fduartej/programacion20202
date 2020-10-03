88c67bfb5f02eaa68faac49bf587557826b23c68
Fred Duarte <gestion_atlassian@inteligogroup.com>
2020-10-03 09:13:17 -0500

    Clase Calculadora

---
+++ calculadoramvc/Models/Calculadora.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace calculadoramvc.Models
+{
+    public class Calculadora
+    {
+        public int Operador1 { get; set; }
+        public int Operador2 { get; set; }
+
+        public String Acccion { get; set; }
+
+        public int Respuesta { get; set; }
+
+    }
+}
