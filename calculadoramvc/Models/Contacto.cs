8164b74254015e9909eceb5a9f2137edaf41d9ae
Fred Duarte <gestion_atlassian@inteligogroup.com>
2020-10-17 10:42:30 -0500

    Fix table name

--- calculadoramvc/Models/Contacto.cs
+++ calculadoramvc/Models/Contacto.cs
@@ -1,8 +1,10 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace calculadoramvc.Models
 {
+    [Table("t_contacto")]
     public class Contacto
     {
         [Required(ErrorMessage = "Por favor ingrese Nombre")]
